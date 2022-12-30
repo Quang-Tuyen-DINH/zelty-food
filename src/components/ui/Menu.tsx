95ff51dca96e6b6cff421109d775cf409f67cbbb
Quang Tuyen DINH <qtuyen43@gmail.com>
2022-12-30 18:29:27 +0100

    Highlight menu badge on selected

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import { Badge } from "./Badge";
 import { MenuStyled } from "../styles/Menu.syled";
 import { Menu } from "../../shared/models/Menu.model";
 
 export const MenuList = (props: {menu: Menu[], selectMenu: any}) => {
+  const[selectedMenu, setSelectedMenu] = useState("M01");
+
   const selectMenu = (event: any) => {
-    props.selectMenu(event.target.id)
+    props.selectMenu(event.target.id);
+    setSelectedMenu(event.target.id);
   }
 
   return (
     <MenuStyled className="zelty-restaurant__menu">
       {props.menu.map((menu: Menu) => (
-        <Badge key={menu.id} id={menu.id} dark onClick={selectMenu}>{menu.name}</Badge>
+        (selectedMenu === menu.id ? 
+        <Badge active dark key={menu.id} id={menu.id} onClick={selectMenu}>{menu.name}</Badge>
+        :
+        <Badge dark key={menu.id} id={menu.id} onClick={selectMenu}>{menu.name}</Badge>)
       ))}
     </MenuStyled>
   );
