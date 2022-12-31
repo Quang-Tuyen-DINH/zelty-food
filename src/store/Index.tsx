aec11b3ca5b6051c6c90d286bcfe4bf869f6285c
Quang Tuyen DINH <qtuyen43@gmail.com>
2022-12-31 22:43:35 +0100

    Finish localStorage

--- src/store/Index.tsx
+++ src/store/Index.tsx
@@ -1,6 +1,12 @@
 import { createStore } from 'redux';
+import LocalStorageService from '../services/local-storage/LocalStorage.service';
 import Reducer from "./Reducer";
 
-const Store = createStore(Reducer);
+const persistedState = LocalStorageService.getStateFromLocal();
+const Store = createStore(Reducer, persistedState);
+
+Store.subscribe(() => {
+  LocalStorageService.saveState(Store.getState())
+})
 
 export default Store;
